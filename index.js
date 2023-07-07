ae66f1a2f2a46be1f766057bbaeb8ad9e9a93bbe
Gabrielkanyi <gabriel.kanyi@student.moringaschool.comm>
2023-07-07 07:53:58 +0300

    changes

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-fetch('https://docs.impala.travel/docs/booking-api/').then((data)=>{
-    console.log(data);
-                return data.json();
-    })
+// const url ='https://docs.impala.travel/docs/booking-api/';
+// const options ={
+//   method :'GET' ,
+// }
+const db = "https://docs.impala.travel/docs/booking-api/"
 
+document.addEventListener("DOMContentLoaded", () => {
+    getHotels();
+    document.querySelector("#book-hotel").addEventListener("click", handleBookTicket);
+});
+
+function getHotels() {
+    fetch(db)
+    .then(res => res.json())
+    .then(hotel => {
+        hotel.forEach(hotel => {renderHotelList(hotel)})
+        const firstHotel = document.querySelector("#id1");
+        firstHotel.dispatchEvent(new Event("click"));
+    })
+}
